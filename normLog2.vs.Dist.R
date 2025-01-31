75dac5b71357565a0964dfedcca00ba0177c4b6a
oashna <130946929+oashna@users.noreply.github.com>
2025-01-31 13:50:15 +0900

    Update normLog2.vs.Dist.R

--- normLog2.vs.Dist.R
+++ normLog2.vs.Dist.R
@@ -1,23 +1,24 @@
 getwd()
-setwd("/Users/oashna/Documents/Shira_lab/PhD/thesis/review/scc2.scc1.ratio")
 
 #calculate distance
 cen <- read.table("cen.bed", stringsAsFactors = F)
 bed$mid <- 1/2 * (bed$start + bed$end)
 cen$mid <- 1/2 * (cen$V2 + cen$V3)
 head(cen)
+
 dist <- numeric(0)
-for (i in c(1:966)){
+for (i in c(1:nrow(bed)){
   temp <- which(cen[,1] == bed[i,1])
   dist <- c(dist, (bed[i,21]-cen[temp,4]))
 }
 bed$dist <- dist
 head(bed)
+
 plot <- ggplot(bed, aes(x=dist, y=normWt)) +
         geom_point() +
         ylim(-0.5,6) +
-  xlim(-500000,500000) +
-  stat_summary_bin(fun='mean', bins=40,
+        xlim(-500000,500000) +
+        stat_summary_bin(fun='mean', bins=40,
                    color='red', linewidth=0.7, geom='line')
 plot
 
